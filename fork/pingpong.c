7b4fff26da125ca619b92e9e4666568972fe3653
nicolas zulaica <nzulaica@fi.uba.ar>
2021-09-18 21:16:12 -0300

    pingpong

--- fork/pingpong.c
+++ fork/pingpong.c
@@ -1,58 +1,74 @@
-#include <stdio.h>
-#include <unistd.h>
-#include "util.h"
 
+#include "util.h"
+#include <stdlib.h>
+#include <time.h>
 
 int
 main()
 {
+	srandom(time(NULL));
+
+	printf( "Hola, soy PID %d: \n", getpid() );
 
 	int fds1 [2];
 	_pipe( fds1 );
+	printf( "  - primer pipe me devuelve: [%d, %d] \n", fds1[0], fds1[1] );
 
 	int fds2 [2];
 	_pipe( fds2 );
+	printf( "  - segundo pipe me devuelve: [%d, %d] \n", fds2[0], fds2[1] );
+
+	printf("\n");
 
 	int readFd;
 	int writeFd;
-
 	int msg = 0;
-	int re_msg = 0;
 
 	int i = _fork();
 
 	if (i == 0) {
-		/// CHILD
-		printf("Soy el proceso hijo y mi pid es: %d\n", getpid());
+		/// CHILD ------------------------- CHILD
 		readFd = selectReadFd( fds1 );
 		writeFd = selectWriteFd( fds2 );
 		
-		msg = 101;
-
-		printf("[Child] WRITE: %i\n", msg );
+		_read( readFd, &msg, sizeof(msg) );
+		
+		printf( "Donde fork me devuelve %d \n", i );
+		printf( "  - getpid me devuelve: %d \n", getpid() );
+		printf( "  - getppid me devuelve: %d \n", getppid() );
+		printf( "  - recibo el valor %d a travez de fd=%d \n", msg, readFd );
+		printf( "  - reenvio el valor en fd=%d \n", writeFd );
+		
 		_write( writeFd, &msg, sizeof(msg) );
 
-		_read( readFd, &re_msg, sizeof(re_msg) );
-		printf("[Child] READ: %i\n", re_msg );
+		printf("\n");
 		
 	} else {
-		/// PARENT
-		printf("Soy el proceso padre y mi pid es: %d\n", getpid());
+		/// PARENT ------------------------- PARENT
 		readFd = selectReadFd( fds2 );
 		writeFd = selectWriteFd( fds1 );
 
-		msg = 123;
+		msg = random();
 
-		_read( readFd, &re_msg, sizeof(re_msg) );
-		printf("[Parent] READ: %i\n", re_msg );
+		printf( "Donde fork me devuelve %d \n", i );
+		printf( "  - getpid me devuelve: %d \n", getpid() );
+		printf( "  - getppid me devuelve: %d \n", getppid() );
+		printf( "  - random me devuelve: %d \n", msg );
+		printf( "  - envio el valor %d a travez de fd=%d \n", msg, writeFd );
 
-		printf("[Parent] WRITE: %i\n", msg );
 		_write( writeFd, &msg, sizeof(msg) );
+		
+		printf("\n");
+
+		_read( readFd, &msg, sizeof(msg) );
+
+		printf( "Hola, de nuevo PID %d \n", getpid() );
+		printf( "  - recibí valor %d vía fd=%d \n", msg, readFd );
 
 	}
 
 	close( writeFd );
 	close( readFd );
-	printf("Terminando\n");
+
 	_exit(0);
 }
