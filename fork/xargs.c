5cc40cf6b4944ce17257f80f7f7eee7519a85575
nicolas zulaica <nzulaica@fi.uba.ar>
2021-09-23 19:53:22 -0300

    xargs

--- fork/xargs.c
+++ fork/xargs.c
@@ -1,7 +1,60 @@
+#include "util.h"
+#include <string.h>
+
+#ifndef NARGS
+#define NARGS 4
+#endif
+
 int
 main(int argc, char *argv[])
 {
-	// Your code here
+	if( argc != 2 ){
+		errno = EINVAL;
+		perror("Interfaz: xargs <comando> \n");
+		_exit(-1);
+	}
 
-	return 0;
+	const maxnargs = NARGS+2;
+
+	char **args= malloc(sizeof(char*) * (maxnargs));
+	for( int i=0; i<maxnargs-1; i++ ) args[i]= malloc(sizeof(char));
+	strcpy(args[0],argv[1]);
+	args[maxnargs]=NULL;
+
+	int nargs = 1;
+
+	char* line = NULL;
+	size_t size;
+
+	int j=1;
+
+	while( getline(&line, &size, stdin ) != -1 ){
+		
+		if(line[strlen(line)-1]=='\n')line[strlen(line)-1] = '\0';
+		strcpy(args[nargs++],line);
+
+		if(nargs > NARGS){
+			args[nargs]=NULL;
+
+			if( !_fork() ){ // CHILD
+				execvp(argv[1],args);
+			}
+			wait(NULL);
+			nargs=1;
+		}
+	};
+	if( nargs > 1 ){
+		args[nargs]=NULL;
+
+		if( !_fork() ){ // CHILD
+			execvp(argv[1],args);
+		}
+		wait(NULL);
+	}
+
+	free(line);
+	for( int i=0; i<NARGS; i++ ) free(args[i]);
+	free(args);
+
+	_exit(0);
 }
