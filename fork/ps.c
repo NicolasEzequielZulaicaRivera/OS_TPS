e1d49841a7e342f7e1fd50609826197653cfa6e5
nicolas zulaica <nzulaica@fi.uba.ar>
2021-09-24 16:35:25 -0300

    ps

--- fork/ps.c
+++ fork/ps.c
@@ -1,7 +1,45 @@
-#include <stdio.h>
+#define _GNU_SOURCE
+#include "util.h"
+#include <string.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+typedef char string[64];
 
 int
-main(int argc, char *argv[])
+main()
 {
-    printf("Hello World!");
+    // El directorio /procs contiene todos los procesos
+    DIR *procs = opendir("/proc");
+
+	struct dirent* entry;
+    while ( ( entry = readdir(procs) ) ) {
+    
+        // Los procesos seran accesibles en forma de directorio dentro de /procs
+        // Los directoris de los procesos se llaman por su PID, 
+        // entonces filtramos los directorios con nombre numerico
+		if (entry->d_type == DT_DIR && atoi(entry->d_name) ) {
+            string line;
+
+            // El archivo stat dentro del directorio de cada proceso 
+            // contiene informacion del proceso
+            string stats = "/proc/";
+            strcat(stats, entry->d_name);
+            strcat(stats, "/stat");
+
+            int fd = open( stats, DT_REG );
+
+            _read( fd, &line, sizeof(line) );
+
+            // Mostramos la linea, que contendra:
+            // PID nombre estado PPID ...etc
+            // el significado del resto de los datos se puede encontrar en:
+            // man 5 proc , /proc/[pid]/stat
+            printf( "%s\n", line );
+
+            close(fd);
+        }
+    }
+
+    closedir( procs );
 }
