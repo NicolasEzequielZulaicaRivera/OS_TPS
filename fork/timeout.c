a790a193ffcfd1bb8e8cad11ec4b4dde7e0ebede
nicolas zulaica <nzulaica@fi.uba.ar>
2021-09-24 16:48:21 -0300

    timeot ini

--- fork/timeout.c
+++ fork/timeout.c
@@ -1,7 +1,19 @@
-#include <stdio.h>
+#include "util.h"
 
 int
 main(int argc, char *argv[])
 {
-    printf("Hello World!");
+    // Crear un timer
+
+    // Fork
+    // El hijo ejecuta el segundo proceso
+    int cpid = _fork();
+
+    if( !cpid ){
+        execvp( argv[2], argv+2 );
+    }
+
+    // Esperar timer
+    // si el hijo no termino lo maa
+    _exit(0);
 }
