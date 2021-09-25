52b356153999e3ef97f5d80fc1c9918314557989
Nicolas Zulaica <nzulaica@fi.uba.ar>
2021-09-25 12:56:47 -0300

    timeout con timer

--- fork/timeout.c
+++ fork/timeout.c
@@ -2,8 +2,11 @@
 #include <signal.h>
 #include <time.h>
 
+// --------------- TIMEOUT CON SLEEP ---------------
+// Utiliza fork, sleep y kill
+/*
 int
-timeout(int argc, char *argv[])
+main (int argc, char *argv[])
 {
     if( argc < 3 ){
         errno = EINVAL;
@@ -27,32 +30,67 @@ timeout(int argc, char *argv[])
     wait(NULL);
     _exit(0);
 }
+*/
+
+// --------------- TIMEOUT CON TIMER ---------------
+// Utiliza timer_create y timer_settime
+// Ademas de fork y kill
+
+// PID HIJO
+// Es global para poder pasarselo al handler
+int child_pid = 0; 
+
+// HANDLER PARA TERMINAR PROCESO HIJO
+void handle_killChild( int sig ){
+    kill(child_pid,SIGKILL);
+}
 
 int
-main()
+main(int argc, char *argv[])
 {
-   // PROBANDO TIMER
-   struct sigevent event;
-   event.sigev_notify = SIGEV_SIGNAL;
-   event.sigev_signo = SIGKILL;
+    // COMPROBAR PARAMETROS
+    if( argc < 3 ){
+        errno = EINVAL;
+		perror("Interfaz: timeout <n> <comando> \n");
+		_exit(-1);
+    }
 
-   struct itimerspec itime;
-   itime.it_value.tv_sec  = 5; 
-   itime.it_value.tv_nsec  = 0; 
-   itime.it_interval.tv_sec = 0;
-   itime.it_interval.tv_nsec  = 0; 
+    // DEFINIR ACCION PARA TERMINAR PROCESO HIJO
+    // Ligamos el handler que termina al proceso hijo a la senal SIGUSR1
+    // No hay problema en usar esta senal, esta para ser usada por los usuarios.
+    struct sigaction killChild;
+    killChild.sa_handler = &handle_killChild;
+    sigaction(SIGUSR1, &killChild, NULL);
 
-   timer_t timerId;
+    // CREAR EVENTO
+    // El evento manda una senal SIGUSR1
+    // Esta senal esta definida para matar al proceso hijo
+    struct sigevent event;
+    event.sigev_notify = SIGEV_SIGNAL;
+    event.sigev_signo = SIGUSR1;
 
-   timer_create( CLOCK_REALTIME , &event, &timerId);
+    // CREAR OBJETO DE TIEMPO
+    // Setear duracion
+    struct itimerspec itime;
+    itime.it_value.tv_sec  = atoi( argv[1] ); 
+    itime.it_value.tv_nsec  = 0;
+    // Setear repeticion - 0 Indica que no se repite
+    itime.it_interval.tv_sec = 0;
+    itime.it_interval.tv_nsec  = 0; 
 
-   timer_settime(timerId, 0, &itime, NULL);
+    // CREAR TIMER
+    timer_t timerId;
+    timer_create( CLOCK_REALTIME , &event, &timerId);
 
+    // INICIAR TIMER
+    timer_settime(timerId, 0, &itime, NULL);
 
-   for( int i=0; i<20; i++ ){
-       printf("%i ",i);
-       fflush(stdout);
-       sleep(1);
-   }
+    // FORK
+    child_pid = _fork();
+    // HIJO : Ejecuta el comando
+    if( !child_pid ) execvp( argv[2], argv+2 );
+    // PADRE : Espera a que el hijo termine ( incluido haberlo terminado )
+    wait(NULL);
+
     return 0;
 }
