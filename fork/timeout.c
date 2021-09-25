a3ac831d60ae8730f7fb2ce457e3b8fac4193186
Nicolas Zulaica <nzulaica@fi.uba.ar>
2021-09-25 11:30:16 -0300

    timeout con sleep

--- fork/timeout.c
+++ fork/timeout.c
@@ -1,19 +1,31 @@
 #include "util.h"
+#include <signal.h>
+#include <time.h>
 
 int
 main(int argc, char *argv[])
 {
-    // Crear un timer
 
-    // Fork
-    // El hijo ejecuta el segundo proceso
-    int cpid = _fork();
-
-    if( !cpid ){
-        execvp( argv[2], argv+2 );
+    if( argc < 3 ){
+        errno = EINVAL;
+		perror("Interfaz: timeout <n> <comando> \n");
+		_exit(-1);
     }
 
+    // Hacer Fork
+    // y guardar el pid hijo
+    int cpid = _fork();
+
+    // El proceso hijo ejecutara el comando
+    if( !cpid ) execvp( argv[2], argv+2 );
+
     // Esperar timer
-    // si el hijo no termino lo maa
+    sleep( atoi( argv[1] ) );
+
+    // si el hijo no termino lo termina
+    // mandandole una senal SIGKILL
+    kill(cpid,SIGKILL);
+    wait(NULL);
+
     _exit(0);
 }
