4ba7597b0b98f5b988064120e8e7b2f4f612e6f1
Nicolas Zulaica <nzulaica@fi.uba.ar>
2021-09-25 12:21:40 -0300

    timer test

--- fork/timeout.c
+++ fork/timeout.c
@@ -3,9 +3,8 @@
 #include <time.h>
 
 int
-main(int argc, char *argv[])
+timeout(int argc, char *argv[])
 {
-
     if( argc < 3 ){
         errno = EINVAL;
 		perror("Interfaz: timeout <n> <comando> \n");
@@ -26,6 +25,34 @@ main(int argc, char *argv[])
     // mandandole una senal SIGKILL
     kill(cpid,SIGKILL);
     wait(NULL);
-
     _exit(0);
+}
+
+int
+main()
+{
+   // PROBANDO TIMER
+   struct sigevent event;
+   event.sigev_notify = SIGEV_SIGNAL;
+   event.sigev_signo = SIGKILL;
+
+   struct itimerspec itime;
+   itime.it_value.tv_sec  = 5; 
+   itime.it_value.tv_nsec  = 0; 
+   itime.it_interval.tv_sec = 0;
+   itime.it_interval.tv_nsec  = 0; 
+
+   timer_t timerId;
+
+   timer_create( CLOCK_REALTIME , &event, &timerId);
+
+   timer_settime(timerId, 0, &itime, NULL);
+
+
+   for( int i=0; i<20; i++ ){
+       printf("%i ",i);
+       fflush(stdout);
+       sleep(1);
+   }
+    return 0;
 }
