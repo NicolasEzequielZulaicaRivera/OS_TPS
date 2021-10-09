5bf1f7700d9c1f7649b45f660436e86647cbb7ad
nicolas zulaica <nzulaica@fi.uba.ar>
2021-10-09 19:49:55 -0300

    prepare arrow handler

--- shell/readline.c
+++ shell/readline.c
@@ -3,6 +3,10 @@
 
 static char buffer[BUFLEN];
 
+// Handle character that start w/ escape code
+// Might change buffer ( and thus i )
+void handleEsc(int *i);
+
 // reads a line from the standard input
 // and prints the prompt
 char *
@@ -24,28 +28,49 @@ read_line(const char *promt)
 
 		switch (c)
 		{
-			case EOF: // END OF FILE
-			case 4: // EOF | CTRL+D
-				return NULL;
-			case 27: // ESC
-				// handle arrows
-				break;
-			case 127: // BACKSPACE
-				buffer[--i] = 0;
-				putchar_debug('\b'); putchar_debug(' '); putchar_debug('\b');
-				break;
-			case END_LINE:
-				buffer[i] = END_STRING;
-				putchar_debug(END_LINE);
-				return buffer;
-			default:
-				buffer[i++] = c;
-				putchar_debug(c);
-				break;
+		case EOF: // END OF FILE
+		case 4: // EOF | CTRL+D
+			return NULL;
+		case 27: // ESC
+			handleEsc(&i);
+			break;
+		case 127: // BACKSPACE
+			buffer[--i] = 0;
+			putchar_debug('\b'); putchar_debug(' '); putchar_debug('\b');
+			break;
+		case END_LINE:
+			buffer[i] = END_STRING;
+			putchar_debug(END_LINE);
+			return buffer;
+		default:
+			buffer[i++] = c;
+			putchar_debug(c);
+			break;
 		}
 #ifndef SHELL_NO_INTERACTIVE
 		fflush(stdout);
 #endif
     }
 	return NULL;
+}
+
+void handleEsc(int *i){
+	int c = 0;
+	read (STDIN_FILENO, &c, 1);
+	switch (c)
+	{
+	case '[': // ARROWS
+		read (STDIN_FILENO, &c, 1);
+		switch (c)
+		{
+		case 'A': // UP-ARROW
+			/* code */
+			break;
+		case 'B': // DOWN-ARROW
+			/* code */
+			break;
+		}
+	break;
+	}
+
 }
