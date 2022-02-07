6b74aa5855e20bf2bbd243c980872594cd8c7df9
nicozu <nzulaica@fi.uba.ar>
2022-02-06 22:56:26 -0300

    parse ps

--- fork/ps.c
+++ fork/ps.c
@@ -6,6 +6,13 @@
 
 typedef char string[64];
 
+
+void
+print_process(string line);
+
+char *
+format_state(string state);
+
 int
 main()
 {
@@ -31,16 +38,68 @@ main()
 
             _read( fd, &line, sizeof(line) );
 
-            // Mostramos la linea, que contendra:
-            // PID nombre estado PPID ...etc
-            // el significado del resto de los datos se puede encontrar en:
-            // man 5 proc , /proc/[pid]/stat
-            printf( "%s\n", line );
+            print_process(line);
 
             close(fd);
         }
     }
-
+    printf( "\n" );
     closedir( procs );
     _exit(0);
+}
+
+
+void
+print_process(string line)
+{
+    char *token;
+    printf( "\e[4m                                                            \e[0m\n\n" );
+
+    token = strtok(line, " ");
+    printf( "PID:\t\e[1m%s\e[0m\n", token );
+        
+    token = strtok(NULL, " ");
+    printf( "COMM:\t%s (filename of the executable)\n", token );
+
+    token = strtok(NULL, " ");
+    printf( "STATE:\t%s (%s)\n", token, format_state(token) );
+
+    token = strtok(NULL, " ");
+    printf( "PPID:\t%s (PID of the parent)\n", token );
+
+    token = strtok(NULL, " ");
+    printf( "PGRP:\t%s (group ID)\n", token );
+
+    // Ignore the rest of the line
+    // The value of further tokens is not important for the purpose of this program
+    // > Can be seen on `man 5 proc` : `/proc/[pid]/stat`
+}
+
+char *
+format_state(string state){
+    if( !strcmp(state,"R") )
+        return "Running";
+    if( !strcmp(state,"S") )
+        return "Sleeping in an interruptible wait";
+    if( !strcmp(state,"D") )
+        return "Waiting in uninterruptible disk sleep";
+    if( !strcmp(state,"Z") )
+        return "Zombie";
+    if( !strcmp(state,"T") )
+        return "Stopped (on a signal) or (before Linux 2.6.33) trace stopped";
+    if( !strcmp(state,"t") )
+        return "Tracing stop (Linux 2.6.33 onward)";
+    if( !strcmp(state,"W") )
+        return "Paging (only before Linux 2.6.0)";
+    if( !strcmp(state,"X") )
+        return "Dead (from Linux 2.6.0 onward)";
+    if( !strcmp(state,"x") )
+        return "Dead (Linux 2.6.33 to 3.13 only)";
+    if( !strcmp(state,"K") )
+        return "Wakekill (Linux 2.6.33 to 3.13 only)";
+    if( !strcmp(state,"W") )
+        return "Waking (Linux 2.6.33 to 3.13 only)";
+    if( !strcmp(state,"P") )
+        return "Parked (Linux 3.9 to 3.13 only)";
+    return "UNKNOWN";
 }
